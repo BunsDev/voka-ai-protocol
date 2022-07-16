c1ad3f850254c0d1b47b39a31c09a3eedb0a61d7
ubuntu <ubuntu@localhost.localdomain>
2022-07-16 13:30:57 +0800

    replace data struct

--- backend/storage/nft_storage_uploader.py
+++ backend/storage/nft_storage_uploader.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+from nft_info import nft_info
  
 
 class NFTStorageUploaderError(Exception):
@@ -17,7 +18,6 @@ class nft_storage_uploader:
         self.urls = {
                 'upload':self.base_url + '/upload',
                 'store':self.base_url + '/store',
-                'upload':self.base_url + '/upload',
                 }
 
         self.headers = {'Authorization':'Bearer ' + nft_storage_key}
@@ -35,7 +35,8 @@ class nft_storage_uploader:
 
         Returns:
           int: 0 表示成功，否则表示失败
-          dict:
+          dict: 成功/失败信息
+            成功时：
             'cid': 'ipfs地址'
             'created': '上传时间'
             'type': 'directory'
@@ -75,7 +76,8 @@ class nft_storage_uploader:
 
         Returns:
           int: 0 表示成功，否则表示失败
-          dict:
+          dict: 成功/失败信息
+            成功时：
             'cid': 'ipfs地址'
             'created': '上传时间'
             'type': 'directory'
@@ -94,7 +96,7 @@ class nft_storage_uploader:
         with open(file_path, 'rb') as f:
             return self.upload_file_from_content(file_name, f, file_type)
 
-    def upload_erc1155_nft(self, image_path, nft_name, nft_properties={},nft_file_properties={}):
+    def upload_erc1155_nft(self, image_path, nft_name, nft_properties={}, nft_str_info = {}, nft_file_info = {}):
         """
         Store an ERC-1155-compatible NFT as a collection of content-addressed objects connected by IPFS CID links.
         Params:
@@ -103,57 +105,32 @@ class nft_storage_uploader:
           nft_properties: 
             nft的属性，比如:
               {"background":"orange", "clothes":"vietnum jacket", "eyes":"blue beams", "fur":"robot", "mouth":"grin"}
-          nft_file_properties
-            nft的文件属性，比如:
-              {"videoclip": <video file content>, "avatormesk" <mest>}
+          nft_str_info:
+            nft除了properties外的其他字符串信息，比如：
+            {"author":{"body_author":"a", "head_author":"b"}}
+          nft_file_info
+            nft除了properties外的其他文件信息，比如：
+              {"models": {"head_model":<model file content>, "body_model":<model file content>"}}
         Return:
           int: 0 表示成功，否则表示失败
-          dict:
+          dict: 成功/失败信息
         """
         with open(image_path, 'rb') as f:
-            return self.upload_erc1155_nft_content(f, nft_name,nft_properties,nft_file_properties)
+            return self.upload_erc1155_nft_content(f, nft_name,nft_properties,nft_str_info, nft_file_info)
 
-    def upload_erc1155_nft_content(self, image_content, nft_name, nft_properties={}, nft_file_properties={}):
+    def upload_erc1155_nft_info(self, nft_info_obj):
         """
         Store an ERC-1155-compatible NFT as a collection of content-addressed objects connected by IPFS CID links.
         Params:
-          image_content: nft对应图片文件内容
-          nft_name: nft的名字
-          nft_properties: 
-            nft的属性，比如:
-              {"background":"orange", "clothes":"vietnum jacket", "eyes":"blue beams", "fur":"robot", "mouth":"grin"}
-          nft_file_properties
-            nft的文件属性，比如:
-              {"videoclip": <video file content>, "avatormesk" <mest>}
+          nft_info_obj： nft_info类，通过nft_info类，传递nft信息
         Return:
           int: 0 表示成功，否则表示失败
-          dict:
+          dict: 成功/失败信息
         """
-        if not isinstance(nft_properties, dict) or not isinstance(nft_file_properties,dict):
-            raise NFTStorageUploaderError("type of nft_properties/nft_file_properties are not dict")
-        meta_json_str = '{"name": "' + nft_name + '","image": "undefined",'
-        if len(nft_properties) > 0 or len(nft_file_properties) > 0:
-            meta_json_str += '"properties":{'
+        if not isinstance(nft_info_obj, nft_info):
+            raise NFTStorageUploaderError("type of nft_info must be nft_info")
 
-        if len(nft_properties) > 0:
-            for pro in nft_properties:
-                meta_json_str += '"' + pro + '":"' + nft_properties[pro] + '",'
-        if len(nft_file_properties) > 0:
-            for f_pro in nft_file_properties:
-                meta_json_str += '"' + f_pro + '":"undefined",'
-
-        meta_json_str = meta_json_str[:-1]
-        if len(nft_properties) > 0 or len(nft_file_properties) > 0:
-            meta_json_str += '}'
-        meta_json_str += '}'
-        files = {'meta':(None, meta_json_str), 
-                'image': (nft_name, image_content),
-                }
-        if len(nft_file_properties) > 0:
-            for f_pro in nft_file_properties:
-                files["properties." + f_pro] = (f_pro,nft_file_properties[f_pro])
-        print(files)
-        response = requests.post(self.urls["store"],files=files,headers=self.headers)
+        response = requests.post(self.urls["store"],files=nft_info_obj.generate_nft_storage_requst_dict(),headers=self.headers)
         if response.status_code == 200:
             try:
                 json = response.json()
@@ -167,11 +144,41 @@ class nft_storage_uploader:
         else:
             return -1, response.text
 
-    def upload_nft(self):
+
+
+    def upload_erc1155_nft_content(self, image_content, nft_name, nft_properties={}, nft_str_info = {}, nft_file_info = {}):
         """
-        自定义nft上传
+        Store an ERC-1155-compatible NFT as a collection of content-addressed objects connected by IPFS CID links.
+        Params:
+          image_content: nft对应图片文件内容
+          nft_name: nft的名字
+          nft_properties: 
+            nft的属性，比如:
+              {"background":"orange", "clothes":"vietnum jacket", "eyes":"blue beams", "fur":"robot", "mouth":"grin"}
+          nft_str_info:
+            nft除了properties外的其他字符串信息，比如：
+            {"author":{"body_author":"a", "head_author":"b"}}
+          nft_file_info
+            nft除了properties外的其他文件信息，比如：
+              {"models": {"head_model":<model file content>, "body_model":<model file content>"}}
+        Return:
+          int: 0 表示成功，否则表示失败
+          dict: 成功/失败信息
         """
-        pass
+        if not isinstance(nft_properties, dict) or not isinstance(nft_str_info,dict) or not isinstance(nft_file_info, dict):
+            raise NFTStorageUploaderError("type of nft_properties/nft_file_properties are not dict")
+        nft_info_obj = nft_info(nft_name, image_content);
+        nft_info_obj.add_properties(nft_properties)
+        nft_info_obj.add_nft_str_info(nft_str_info)
+        nft_info_obj.add_nft_file_info(nft_file_info)
+        return self.upload_erc1155_nft_info(nft_info_obj)
+
+    def upload_nft(self, image_path, nft_name, nft_properties={},nft_file_properties={}):
+        """
+        自定义nft上传，目前暂时使用self.upload_erc1155_nft。
+        TODO: 改成自定义
+        """
+        return self.upload_erc1155_nft(image_path, nft_name, nft_properties,nft_file_properties)
 
     def list(self,before, limit = 10):
         """
@@ -181,6 +188,9 @@ class nft_storage_uploader:
         Params:
           before: string($date-time), eg. 2020-07-27T17:32:28Z
           limit: int
+        Return:
+          int: 0 表示成功，否则表示失败
+          dict: 成功/失败信息
         """
         list_url = self.base_url + "/?before=" + before + "&limit=" + str(limit)
         response = requests.get(list_url,headers=self.headers)
@@ -199,7 +209,8 @@ if __name__ == "__main__":
     # usage demos
     nftL = nft_storage_uploader()
     with open("head.png", 'rb') as f:
-        err_code, json = nftL.upload_erc1155_nft('./head.png','resss',nft_properties={"aaa":"bbb"},nft_file_properties = {"videoClip": f})
+        #err_code, json = nftL.upload_erc1155_nft('./head.png','resss')
+        err_code, json = nftL.upload_erc1155_nft('./head.png','resss',nft_properties={"aaa":"bbb"},nft_file_info = {"test":{"videoClip": f}})
     #err_code, json = nftL.list("2022-07-14T17%3A32%3A28Z",10)
     #err_code, json = nftL.upload_file_from_path("./head.png","image/png")
     if err_code == 0:
